1db9280d773a972ee814e3d42408288b20ee3038
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-17 14:36:29 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Assignment_7._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = "ab";
+            string word2 = "pqrs";
+            int i = 0;
+        
+
+            int wordLength = Math.Min(word1.Length, word2.Length);
+
+            while (i < wordLength) 
+            {
+                Console.Write(word1[i]);
+                Console.Write(word2[i]);
+                i++;
+            }
+            if (word1.Length > wordLength) 
+            {
+                Console.WriteLine(word1[i..]);
+            }
+            else if (word2.Length > wordLength) 
+            {
+                Console.WriteLine(word2[i..]);
+            }
+
+            
+            
+
+        }
+    }
+}
